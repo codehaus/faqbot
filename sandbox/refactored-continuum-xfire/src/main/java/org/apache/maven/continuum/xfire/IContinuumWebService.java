1ea25f978b10777ac49970ee7e6161a22aaeb4f1
rahul <rahul@0e148b70-6efd-0310-ad2c-90324b823317>
2006-04-29 01:07:55 +0000

    Updates

--- sandbox/refactored-continuum-xfire/src/main/java/org/apache/maven/continuum/xfire/IContinuumWebService.java
+++ sandbox/refactored-continuum-xfire/src/main/java/org/apache/maven/continuum/xfire/IContinuumWebService.java
@@ -9,6 +9,7 @@ import org.apache.maven.continuum.Continuum;
  * Contract for operations allowed by Continuum Web Service implementations.
  * 
  * @author <a href='mailto:rahul.thakur.xdev@gmail.com'>Rahul Thakur</a>
+ * @deprecated <em>experimental</em>
  */
 public interface IContinuumWebService extends Continuum {
 
